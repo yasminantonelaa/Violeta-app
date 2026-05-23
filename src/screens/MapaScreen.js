447dfc8236489d9de9321a6847dfea1beb70260d
Markus <markusaurelius04@gmail.com>
2026-05-23 14:26:22 -0300

    Eliminate unused Callout import from MapaScreen.js
    Removed the Callout component import as it is not being used in the MapaScreen.

--- src/screens/MapaScreen.js
+++ src/screens/MapaScreen.js
@@ -17,7 +17,7 @@ import {
   Alert, TextInput, Modal, ScrollView, ActivityIndicator, FlatList, Animated
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import MapView, { Marker, Callout } from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 // -- Configuração da API JSONBin --
